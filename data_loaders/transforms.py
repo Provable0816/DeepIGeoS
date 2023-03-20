5b8b885c719f74deab101c905f332d400fb39706
Sizheng Guo <745134809@qq.com>
2023-03-20 13:05:14 +0800

    fixed bugs when using UNet

--- data_loaders/transforms.py
+++ data_loaders/transforms.py
@@ -7,6 +7,7 @@ def get_transform(split):
         transform = [
             tio.ToCanonical(),
             tio.Resample(1),
+            # 如果是 unet 需要将crop大小设置为一个合适的大小，以便 unet 下采样后能恢复到原始crop的大小
             tio.CropOrPad(mask_name='crop_mask'), # crop only object region
             tio.RandomAffine(degrees=[-np.pi/8, np.pi/8], scales=[0.8, 1.25]),
             tio.RandomFlip(axes=(0, 1, 2)),
@@ -16,6 +17,8 @@ def get_transform(split):
         transform = [
             tio.ToCanonical(), 
             tio.Resample(1),
+            # 如果是 unet 需要将crop大小设置为一个合适的大小，以便 unet 下采样后能恢复到原始crop的大小
+            tio.CropOrPad(mask_name='crop_mask'), # crop only object region
             tio.RemapLabels({2:1, 3:1, 4:1}),
         ]
     else:
